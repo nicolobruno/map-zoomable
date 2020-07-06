a0788fd6f5af800dfc249ad84849526302bf264e
nicolas <nicolas@cookunity.com>
2020-07-06 00:23:54 -0300

    Correction npm i

---
+++ build/precache-manifest.a58e425e04c538610f56ca7b836064bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fbea608dd4541dd19b65b0650af8e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90d4d4d1ecc0369890ec",
+    "url": "/static/css/main.44de730c.chunk.css"
+  },
+  {
+    "revision": "2341cd0bde4d73c617b3",
+    "url": "/static/js/2.5d5c2a7d.chunk.js"
+  },
+  {
+    "revision": "90d4d4d1ecc0369890ec",
+    "url": "/static/js/main.b381a26c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
